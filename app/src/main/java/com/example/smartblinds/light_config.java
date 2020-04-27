a84a5e82cef490489cc224ad6f7be42fc15bc993
Itsuki Sakamoto <isakamot>
2020-04-27 01:24:17 -0400

    Done with Light Configuration stuff

--- app/src/main/java/com/example/smartblinds/light_config.java
+++ app/src/main/java/com/example/smartblinds/light_config.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
-import android.widget.Adapter;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
